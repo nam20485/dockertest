fe5ee23b831404c2c18c0baaffccef15a83f89be
nam20485 <nmiller217@gmail.com>
2017-11-19 14:42:13 -0800

    add eb commands

--- deploy/deploy_ebs.sh
+++ deploy/deploy_ebs.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+eb init --profile <YOUR AWS PROFILE NAME>
+
 # create elastic-beanstalk environment (?)
+eb create <YOUR ENVIRONMENT NAME> --profile <YOUR AWS PROFILE NAME> -i t2.small -p "multi-container-docker-1.11.2-(generic)" -s -k <YOUR AWS KEYPAIR NAME> --envvars "NODE_ENV=localdev,SKIP_PASSPORT=true,SKIP_UPLOAD=true,SKIP_OG=true"  
 
 # update elastic-beanstalk app with newly-created image from ECR
+eb deploy
