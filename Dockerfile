ac8988e197f66485a69b55e2b9b27a60c6d08e63
nam20485 <nmiller217@gmail.com>
2017-11-20 14:57:17 -0800

    changes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
- FROM python:3
+ FROM amazon/aws-eb-python:3.4.2-onbuild-3.5.1
  ENV PYTHONUNBUFFERED 1
+ EXPOSE 8000
+ 
  RUN mkdir /code
  WORKDIR /code
  ADD requirements.txt /code/
  RUN pip install -r requirements.txt
  ADD . /code/
- EXPOSE 80
--- deploy/push_image.sh
+++ deploy/push_image.sh
@@ -2,11 +2,14 @@
 
 set -x
 
+# build docker image
 docker build -t dockertest .
 
 # output contains the docker login command, send it to bash to execute
 aws ecr get-login --no-include-email | bash
 
+# tag our docker image
 docker tag dockertest:latest 903455642405.dkr.ecr.us-west-2.amazonaws.com/dockertest:latest
 
+# push docker image to AWS ECS repo
 docker push 903455642405.dkr.ecr.us-west-2.amazonaws.com/dockertest:latest
