a25eb9e3d12730ca7bfe1b3bb2bf2cc28ff34356
nam20485 <nmiller217@gmail.com>
2017-11-20 13:28:51 -0800

    changes

--- deploy/deploy_ebs.sh
+++ deploy/deploy_ebs.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-eb init --profile <YOUR AWS PROFILE NAME>
+#eb init --profile <YOUR AWS PROFILE NAME>
 
 # create elastic-beanstalk environment (?)
-eb create <YOUR ENVIRONMENT NAME> --profile <YOUR AWS PROFILE NAME> -i t2.small -p "multi-container-docker-1.11.2-(generic)" -s -k <YOUR AWS KEYPAIR NAME> --envvars "NODE_ENV=localdev,SKIP_PASSPORT=true,SKIP_UPLOAD=true,SKIP_OG=true"  
+#eb create <YOUR ENVIRONMENT NAME> --profile <YOUR AWS PROFILE NAME> -i t2.small -p "multi-container-d3ocker-1.11.2-(generic)" -s -k <YOUR AWS KEYPAIR NAME>
 
 # update elastic-beanstalk app with newly-created image from ECR
 eb deploy
--- deploy/push_image.sh
+++ deploy/push_image.sh
@@ -4,6 +4,7 @@ set -x
 
 docker build -t dockertest .
 
+# output contains the docker login command, send it to bash to execute
 aws ecr get-login --no-include-email | bash
 
 docker tag dockertest:latest 903455642405.dkr.ecr.us-west-2.amazonaws.com/dockertest:latest
